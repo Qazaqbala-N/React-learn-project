db44ce3a45603f1e1792c7f360bd04fabdceb6ce
Nurdaulet Melissov <nurdauletktl2019@gmail.com>
2022-06-18 17:26:14 +0600

    Init:project created

---
+++ src/Redux/state.js
@@ -0,0 +1,21 @@
+
+let state = {
+    dataDialog : [
+        { name: "Nurdaulet", id: 1 },
+        { name: "Asik", id: 2 },
+        { name: "Nurawit", id: 3 },
+        { name: "Kana", id: 4 },
+    ],
+    dataMessage : [
+        { message: "Hi", id: 1 },
+        { message: "What's your name", id: 2 },
+        { message: "YEs?", id: 3 },
+        { message: "Where are you?", id: 4 },
+    ],
+    dataPost : [
+        {message:"Hi, how are you?",id:0,like:39},
+        {message:"It's my first message",id:1,like:23}
+    ]
+}
+
+export  default state;
